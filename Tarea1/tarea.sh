bcf2dbbd0bf77b2cc76372ee6f597d33e8f57f94
elGranCali <cali2910@gmail.com>
2015-08-31 13:55:50 -0600

    Arreglillo del batch

--- Tarea1/tarea.sh
+++ Tarea1/tarea.sh
@@ -2,4 +2,4 @@
 #$ -cwd
 #$ -j y
 #$ -S /bin/bash
-echo 3 | /opt/mpich2/gnu/bin/mpiexec -n 20 -f /home/A75952/distribucion ./primos 
+echo 3 | /opt/mpich2/gnu/bin/mpiexec -n 20 -f /home/A75952/Tareas/distribucion ./primos 
